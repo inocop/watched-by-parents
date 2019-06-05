f3c4d3a61cf44e9452b2299d2429b55b7ead1107
inocop <inoue19851110@gmail.com>
2019-06-06 08:53:31 +0900

    delete Version.ts

--- src/Version.ts
+++
@@ -1,8 +0,0 @@
-import * as path from 'path';
-
-const version = () => {
-  const packageJson = require(path.join(__dirname, '../package.json'));
-  return packageJson.version;
-}
-
-export default version();
